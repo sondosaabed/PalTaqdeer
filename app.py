d8722dea7e0cdc55ece6dcc09ceb989266a63129
Sondos Ahmad Aabed <65151701+sondosaabed@users.noreply.github.com>
2023-09-02 16:22:15 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,13 +3,26 @@ from flask import Flask, render_template, request, jsonify
 import pickle
 import pandas as pd
 import numpy as np
+import mariadb
+import jinja2
+
+conn = mariadb.connect(
+    user="root",
+    password="root",
+    host="localhost",
+    database="pal_taqdeer")
+cur = conn.cursor()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    query = "select * from student_forecast"
+    cur.execute(query)
+    data = list(cur)
+
+    return render_template("index.html", data=data)
 
 
 with open("model.pkl", "rb") as f:
@@ -28,23 +41,39 @@ def result():
     g2 = request.form.get('g2')
     gouout = request.form.get('gouout')
 
-    age_no = age
-    medu_no = medu
-    fedu_no = fedu
-    failures_no = failures
-    higher_no = higher
-    romantic_no = romantic
-    g1_no = g1
-    g2_no = g2
-    hanging = gouout
+    if not age or int(age) < 15 or int(age) > 22:
+        return jsonify({'error' : 'Invalid Age'})
+    if medu == "":
+        return jsonify({'error' : 'Invalid Mother Education Status'})
+    if fedu == "":
+        return jsonify({'error' : 'Invalid Father Education Status'})
+    if failures == "" or int(failures) > 4:
+        return jsonify({'error' : 'Invalid Failures No.'})
+    if not higher:
+        return jsonify({'error' : 'Invalid Higher Education Status'})
+    if not romantic:
+        return jsonify({'error' : 'Invalid Romantic Status'})
+    if g1 == "" or int(g1) > 20:
+        return jsonify({'error' : 'Invalid First Period Grade'})
+    if g2 == "" or int(g2) > 20:
+        return jsonify({'error' : 'Invalid Second Period Grade'})
+    if not int(gouout) or int(gouout) > 5:
+        return jsonify({'error' : 'Invalid Hang Out Status'})
 
-    inputs = [age_no, medu_no, fedu_no, failures_no,
-              higher_no, g1_no, g2_no, romantic_no, hanging]
+    inputs = [age, medu, fedu, failures,
+              higher, g1, g2, romantic, gouout]
     df = pd.DataFrame([np.array(inputs)], columns=[
         'age', 'Medu', 'Fedu', 'failures', 'higher_yes', 'G1', 'G2', 'romantic_no', 'goout'])
 
     finalGrade = model.predict(df)
     final_grade_rounded = round(float(finalGrade))
+
+    query = f""" INSERT INTO student_forecast (age, medu, fedu, failures, higher, romantic, g1, g2, gouout)
+            VALUES ('{age}','{medu}','{fedu}','{failures}','{higher}','{romantic}','{g1}','{g2}','{gouout}'); """
+
+    cur.execute(query)
+    conn.commit()
+
     return jsonify({'grade': final_grade_rounded})
 
 
--- static/assets/js/ajax.js
+++ static/assets/js/ajax.js
@@ -1,6 +1,53 @@
 $(document).ready(function() {
+    $('form').validate({
+        messages: {
+  
+          age: {
+            required: "Required",
+            min: "Minimum 15 Years",
+            max: "Maximum 22 Years",
+        },
+          medu: "Required",
+          fedu: "Required",
+          failures: {
+            required: "Required",
+            min: "Minimum 0 Failures",
+            max: "Maximum 4 Failures",
+        },
+          higher: "Required",
+          romantic: "Required",
+          g1: {
+            required: "Required",
+            min: "Minimum Grade 0",
+            max: "Maximum Grade 20",
+        },
+          g2: {
+            required: "Required",
+            min: "Minimum Grade 0",
+            max: "Maximum Grade 20",
+        },
+          gouout: {
+            required: "Required",
+            min: "Minimum Hang Out 0",
+            max: "Maximum Hang Out 5",
+        },
+  
+        },
+        rules: {
+          age: "required",
+          medu: "required",
+          fedu: "required",
+          failures: "required",
+          higher: "required",
+          romantic: "required",
+          g1: "required",
+          g2: "required",
+          gouout: "required",
+        },
+  
+      });
         
-    $('form').on('submit', function(event) {
+      $('form').on('submit', function(event) {
         $.ajax({
             data : {
                 age : $('#age').val(),
@@ -14,29 +61,46 @@ $(document).ready(function() {
                 gouout : $('#gouout').val(),
             },
             type : 'POST',
-            url : '/result'
-        })
-        .done(function(data) {
-            if (data.grade >= 10) {
-                $('#score').text(data.grade);
-                $('#rslt').text('Congrats !!');
-                $('#score').removeClass();
-                $('#rslt').removeClass();
-                $('#score').addClass('alert alert-success text-center');
-                $('#rslt').addClass('alert alert-success text-center');
-                $('#scss').show()
-                $('#fail').hide()
-            }
-            else {
-                $('#score').text(data.grade);
-                $('#rslt').text('Hard Luck !!');
-                $('#score').removeClass();
-                $('#rslt').removeClass();
-                $('#score').addClass('alert alert-danger text-center');
-                $('#rslt').addClass('alert alert-danger text-center');
-                $('#scss').hide()
-                $('#fail').show()
-
+            url : '/result',
+            success: function(data) {
+                if (data.error){
+                    $('#scss').hide();
+                    $('#fail').hide();
+                    $('#score').hide();
+                    $('#score').removeClass();
+                    $('#rslt').removeClass();
+                    $('#rslt').addClass('alert alert-danger text-center');
+                    // $('#score').text(data.error);
+                    $('#rslt').text(data.error);
+                    trigger.addEventListener("click", toggleModal);
+                    window.addEventListener("click", windowOnClick);          
+                }
+                else if (data.grade >= 10) {
+                    $('#score').text(data.grade);
+                    $('#rslt').text('Congrats !!');
+                    $('#score').removeClass();
+                    $('#rslt').removeClass();
+                    $('#score').addClass('alert alert-success text-center');
+                    $('#rslt').addClass('alert alert-success text-center');
+                    $('#scss').show();
+                    $('#score').show();
+                    $('#fail').hide();
+                    trigger.addEventListener("click", toggleModal);
+                    window.addEventListener("click", windowOnClick);
+                }
+                else if (data.grade < 10) {
+                    $('#score').text(data.grade);
+                    $('#rslt').text('Hard Luck !!');
+                    $('#score').removeClass();
+                    $('#rslt').removeClass();
+                    $('#score').addClass('alert alert-danger text-center');
+                    $('#rslt').addClass('alert alert-danger text-center');
+                    $('#scss').hide();
+                    $('#score').show();
+                    $('#fail').show();
+                    trigger.addEventListener("click", toggleModal);
+                    window.addEventListener("click", windowOnClick);
+                }
             }
         });
         event.preventDefault();
